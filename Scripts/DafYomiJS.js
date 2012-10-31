58a32d73115a7860b87563b2312fbec118211300
Dovi <Dovip@sela.co.il>
2012-10-31 19:29:43 +0200

    Insert files to convert

---
+++ Scripts/DafYomiJS.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+    // Send an AJAX request 
+    var d = new Date();
+    var dateKey = d.getDate() + '' + (d.getMonth() + 1) + '' + d.getFullYear();
+    $.ajax({
+        url: "http://ec2-50-17-140-246.compute-1.amazonaws.com/Daf%20Yomi/api/Pages/GetPageByDate?key=" + dateKey,
+        type: 'GET',
+        dataType: 'jsonp',
+        success: function (data) {
+            if (data != null) {
+                $("#daf").append(data.PageData);
+                $("#date").append(data.Date);
+                $("#dafNumber").append(data.DafNumber);
+            }
+        }
+    });
+    //    $.getJSON("http://ec2-50-17-140-246.compute-1.amazonaws.com/Daf%20Yomi/api/Pages/GetPageByDate?key=" + dateKey,
+    //        function (data) {
+    //            if (data != null) {
+    //                $("#daf").append(data.PageData);
+    //                $("#date").append(data.Date);
+    //                $("#dafNumber").append(data.DafNumber);
+    //            }
+    //        });
+}); 
